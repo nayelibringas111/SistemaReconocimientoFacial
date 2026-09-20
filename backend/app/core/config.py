ccab5c560b8887a022cb8a70e00ad80d4f1625ab
zambra12 <1530834@senati.pe>
2026-09-20 14:50:46 -0500

    data

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,8 +4,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Sistema Reconocimiento Facial"
     GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY", "")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "")  # <- Esta es la línea que falta
 
     class Config:
         env_file = ".env"
+        extra = "ignore"
 
 settings = Settings()
